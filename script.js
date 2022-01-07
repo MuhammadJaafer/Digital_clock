e1bbc7c0154145b7755e479b29135a292770c3fb
Muhammadjaafer <mohamadjafer14235@gmail.com>
2022-01-07 21:11:28 +0200

    workin ot color changing

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const secondeElement = document.querySelector(".second");
 const backgroundColorButton = document.querySelector(".background_button");
 const numberColorButton = document.querySelector(".numbers_button");
 const fontStyleButton = document.querySelector(".font_button");
+const colorPiker = document.querySelector(".colorPiker");
+const colorPikerDisplay = document.querySelector(".colorPikerhide");
+const colorChose = document.querySelectorAll(".color");
 //clock
 setInterval(() => {
   const now = new Date();
@@ -15,5 +18,15 @@ setInterval(() => {
 //clock
 
 //background color
-
+backgroundColorButton.addEventListener("click", function () {
+  colorPikerDisplay.style.display = "grid";
+});
+numberColorButton.addEventListener("click", function () {
+  colorPikerDisplay.style.display = "grid";
+});
+colorChose.forEach((element) => {
+  element.addEventListener("click", function () {
+    colorPikerDisplay.style.display = "none";
+  });
+});
 //background color
